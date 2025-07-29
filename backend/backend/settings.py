fff94f14d57eeb6bcfc66262b0ba777ec951f4d4
Nathan Manicoba <zaphiroxd5@gmail.com>
2025-07-29 13:31:52 -0300

    coreção de algumas coisas

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,4 +129,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = [
+    "http://3.93.178.115",  # ou o DNS depois
+    "http://localhost:3000",  # se estiver testando localmente
+]
